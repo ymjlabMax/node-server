b966b9e072335199871d68c45fdc812b004f5256
Max <sud665@ymjlab.com>
2022-10-21 11:10:59 +0900

    노드서버 구축

--- node_https/app.js
+++ node_https/app.js
@@ -8,15 +8,21 @@ var session = require("express-session");
 var express = require("express");
 var app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(
+    cors({
+        origin: ["https://localhost:3000"],
+        credentials: true,
+        methods: ["GET", "POST", "OPTIONS", "DELETE", "PATCH"],
+    })
+);
 
 var routes = require("./route.js");
 app.use("/", routes);
 
-var server = app.listen(SERVER_PORT, "0.0.0.0", function () {
+var server = app.listen(SERVER_PORT, "127.0.0.1", function () {
     var host = server.address().address;
     var port = server.address().port;
 
-  
+    console.log(server.address());
     console.log("app listening at http://%s:%s", host, port);
 });
--- node_https/route.js
+++ node_https/route.js
@@ -1,37 +1,38 @@
-
-var async = require('async');
-var express = require('express');
+var async = require("async");
+var express = require("express");
 var router = express.Router();
-var bodyParser = require('body-parser');
-var request = require('request');
-var mysql = require('mysql');
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(bodyParser.json())
+var bodyParser = require("body-parser");
+var request = require("request");
+var mysql = require("mysql");
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
-var fs = require('fs');
+var fs = require("fs");
 console.log(__dirname);
 
 var pool = mysql.createPool({
-	host :'localhost',
-	port : 3306,
-	user : 'wordpress',
-	password : 'oR4FpTAd',
-	database:'SHOPYMJ',
-	connectionLimit:50,
-	waitForConnections:false,
-   multipleStatements: true,
-	dateStrings: 'date',
-	timezone: '+09:00',
-	dialectOptions: {
-	 timezone: "+09:00", // DB에서 가져올 때 시간 설정
-   }
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1q2w3e4r",
+    database: "SHOPYMJ",
+    connectionLimit: 50,
+    waitForConnections: false,
+    multipleStatements: true,
+    dateStrings: "date",
+    timezone: "+09:00",
+    dialectOptions: {
+        timezone: "+09:00", // DB에서 가져올 때 시간 설정
+    },
 });
 
 function cyrb128(str, date) {
     // let date = new Date();
 
-    let h1 = (1779033703 - date), h2 = 3144134277,
-        h3 = 1013904242, h4 = 2773480762;
+    let h1 = 1779033703 - date,
+        h2 = 3144134277,
+        h3 = 1013904242,
+        h4 = 2773480762;
 
     for (let i = 0, k; i < str.length; i++) {
         k = str.charCodeAt(i);
@@ -44,7 +45,7 @@ function cyrb128(str, date) {
     h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
     h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
     h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
-    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+    return [(h1 ^ h2 ^ h3 ^ h4) >>> 0, (h2 ^ h1) >>> 0, (h3 ^ h1) >>> 0, (h4 ^ h1) >>> 0];
 }
 
 /*
@@ -66,60 +67,64 @@ CREATE TABLE `shop` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 |
 */
 
-
-router.get('/test', function(req, res) {
-	res.send('ok');
+router.get("/test", function (req, res) {
+    res.send("ok");
 });
 
-router.get('/shopHistory', function(req, res) {
-	var authKey = req.query.authKey;	//'3947429038';
-	var nickName = req.query.nickName; //'alfie96';
-	var day = req.query.baseday;
-	console.log(nickName, day, authKey);
+router.post("/shopHistory", function (req, res) {
+    var authKey = req.body.authKey; //'3947429038';
+    var nickName = req.body.nickName; //'alfie96';
+    var day = req.body.baseDay;
 
-	if( checkBlank(authKey) || checkBlank(nickName) || checkBlank(day)) {
+    console.log(req.body);
+    console.log(nickName, day, authKey);
+
+    if (checkBlank(authKey) || checkBlank(nickName) || checkBlank(day)) {
         res.status(401).send("bad request");
-		console.log('['+nickName+']', '['+day+']', '['+authKey+']');
+        console.log("[" + nickName + "]", "[" + day + "]", "[" + authKey + "]");
         return;
     }
-	var baseday = new Date(day);
-	console.log(nickName, baseday, authKey);
-	var ret = cyrb128(nickName, baseday.getDate());	//3947429038
-	console.log("result("+authKey+")" , ret);
+    var baseday = new Date(day);
+    var ret = cyrb128(nickName, baseday.getDate()); //3947429038
+    console.log("result(" + authKey + ")", ret);
 
-	var rlt = 'N';
-	if( ret[0] == authKey) rlt = 'Y';
+    var rlt = "N";
+    if (ret[0] == authKey) rlt = "Y";
 
-	pool.getConnection(function(err,connection){
-		if( err ) {
-			console.log(err);
-			console.log('return err');
-			res.send(err);
-			return;
-		}
-		connection.query("insert into shop (authKey, userId, baseday, result, regDate) values (?,?,?,?, now())", [authKey, nickName, baseday, rlt], function(err, rows) {
-			if(err) {
-				connection.release();
-				console.log(this.sql);
-				console.log(err);
-				res.send(err);
-				return;
-			}
-			console.log('return ', rlt);
-			res.send(rlt);
-		});
-	});
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+            console.log("return err");
+            res.send(err);
+            return;
+        }
+        connection.query(
+            "insert into shop (authKey, userId, baseDay, result, regDate) values (?,?,?,?, now())",
+            [authKey, nickName, baseday, rlt],
+            function (err, rows) {
+                if (err) {
+                    connection.release();
+                    console.log(this.sql);
+                    console.log(err);
+                    res.send(err);
+                    return;
+                }
+                console.log("return ", rlt);
+                res.send(rlt);
+            }
+        );
+    });
 });
 
-String.prototype.rtrim = function() {
-	return this.replace(/\s+$/,"");
-}
+String.prototype.rtrim = function () {
+    return this.replace(/\s+$/, "");
+};
 
 function checkBlank(str) {
-	if( str == undefined || str.rtrim() == "" ) {
-		return true;
-	}
-	return false;
+    if (str == undefined || str.rtrim() == "") {
+        return true;
+    }
+    return false;
 }
 
 module.exports = router;
